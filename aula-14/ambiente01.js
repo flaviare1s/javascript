ccc12ac4d0b361660f180da4916e6e3b69ba9fe2
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-12 08:36:16 -0300

    Criei ambiente01.js

---
+++ aula-14/ambiente01.js
@@ -0,0 +1,12 @@
+// Estrutura de repetição com variável de controle:
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+
+/* Estrutura de repetição com  teste lógico no início:
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
