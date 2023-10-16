99b2f32dc0464bd38aabd657bbd3aebfb0f23e3a
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-16 09:57:13 -0300

    Criei funcao01.js

---
+++ aula-16/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if(n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+let res = parimpar(7060)
+console.log(res)
+
+/* Ou simplificando:
+console.log(parimpar(11)) 
+*/
