be5b22660c84aeaa96bc7b4608918d1e11a3e9bc
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-16 08:56:28 -0300

    Atualizei ambiente.js

--- aula-15/ambiente.js
+++ aula-15/ambiente.js
@@ -7,3 +7,10 @@ num.push(14)
 console.log(`Nosso vetor é o ${num}. O vetor tem ${num.length} posições.`)
 
 console.log(`O promeiro valor do vetor é ${num[0]}.`)
+
+let pos = num.indexOf(8)
+if(pos == -1) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
