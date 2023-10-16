a81d50b630279cc862b7af1a23f508ded165fa8f
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-16 08:27:45 -0300

    Fiz primeiro exercício da aula 15

---
+++ aula-15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 2, 8, 9, 3]
+
+num.sort()
+
+num.push(14)
+
+console.log(`Nosso vetor é o ${num}. O vetor tem ${num.length} posições.`)
+
+console.log(`O promeiro valor do vetor é ${num[0]}.`)
