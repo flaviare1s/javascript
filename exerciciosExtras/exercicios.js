c5b7e836dd0719af2a4c6d3ff4b2b807867eeed8
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-27 12:51:00 -0300

    Criei uma nova pasta de exercícios

---
+++ exerciciosExtras/exercicios.js
@@ -0,0 +1,8 @@
+// Crie uma função para calcular o fatorial de um número:
+
+function fatorial() {
+    const n = Number(prompt('Digite um número inteiro!'))
+    const x = 1
+
+    for (i = 1; n <= i; i++)
+}
