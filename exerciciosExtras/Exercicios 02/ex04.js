526bcd9598a4cf98494093a356b2beb0f77012f2
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-30 08:42:21 -0300

    Fiz ex04.js

---
+++ exerciciosExtras/Exercicios 02/ex04.js
@@ -0,0 +1,15 @@
+/*
+4. *Laços (Loops):*
+   - Use um loop for para exibir os números de 1 a 10 no console.
+   - Use um loop while para contar de 10 a 1 e exibir no console.
+   */
+
+  for (c = 1; c <= 10; c++) {
+    console.log(c)
+  }
+
+   let i = 10
+   while (i > 0) {
+    console.log(i)
+    i--
+   }
