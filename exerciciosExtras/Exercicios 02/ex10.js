22a5c18cb94419c7b9131ccf9d849743e6e8d052
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-30 12:54:23 -0300

    Fiz ex10.js

---
+++ exerciciosExtras/Exercicios 02/ex10.js
@@ -0,0 +1,9 @@
+/*
+10. *Manipulação de Strings:*
+    - Concatene strings para criar uma frase.
+    - Extraia partes de uma string usando métodos como substring ou slice.
+*/
+
+let frase = 'Essa é uma string de exemplo para o exercício'
+let parte = frase.slice(5,10)
+console.log(parte)
