9eb7745ca5e94150414c2e6287c550e5948847f9
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-30 07:59:10 -0300

    Fiz mais exercícios

--- exerciciosExtras/Exercicios 02/exercicios2.js
+++ exerciciosExtras/Exercicios 02/exercicios2.js
@@ -1,3 +1,5 @@
+/*
+EXERCÍCIOS:
 1. *Variáveis e Tipos de Dados:*
    - Crie uma variável para armazenar seu nome e exiba-a no console.
    - Declare variáveis para idade, altura e status de estudante.
@@ -37,3 +39,22 @@
 10. *Manipulação de Strings:*
     - Concatene strings para criar uma frase.
     - Extraia partes de uma string usando métodos como substring ou slice.
+    */
+
+    /*
+    1. *Variáveis e Tipos de Dados:*
+   - Crie uma variável para armazenar seu nome e exiba-a no console.
+   - Declare variáveis para idade, altura e status de estudante.
+   */
+
+   let nome = prompt('Qual é o seu nome?')
+   console.log(`Nome: ${nome}`)
+   let altura = prompt('Qual a sua altura?')
+   console.log(`Altura: ${altura}m`)
+   let estudante = prompt('Você é estudante? SIM ou NÃO') 
+    if (estudante === 'SIM' || estudante === 'sim' || estudante === 'Sim') {
+        console.log(true) 
+    } else {
+        console.log(false)
+    }
+   
