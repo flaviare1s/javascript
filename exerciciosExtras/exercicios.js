bf0098293d80583a1d013512eeb6a6964bab14d9
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-30 07:46:26 -0300

    Criei novos arquivos de exercícios

--- exerciciosExtras/exercicios.js
+++ exerciciosExtras/exercicios.js
@@ -1,5 +1,5 @@
 // Crie uma função para calcular o fatorial de um número:
-
+/*
 function fatorial() {
     const n = Number(prompt('Digite um número inteiro!'))
     let fat = 1
@@ -10,3 +10,7 @@ function fatorial() {
 }
 
 console.log(fatorial())
+*/
+
+// Calcule a divisão de dois números fatoriais M e N:
+
---
+++ exerciciosExtras/exercicios2.js
@@ -0,0 +1,39 @@
+1. *Variáveis e Tipos de Dados:*
+   - Crie uma variável para armazenar seu nome e exiba-a no console.
+   - Declare variáveis para idade, altura e status de estudante.
+
+2. *Operadores:*
+   - Calcule a soma, subtração, multiplicação e divisão de dois números.
+   - Verifique se um número é par ou ímpar usando operadores condicionais.
+
+3. *Estruturas Condicionais:*
+   - Crie um programa que verifica se a pessoa é maior de idade.
+   - Crie um programa que avalia se uma pessoa pode votar.
+
+4. *Laços (Loops):*
+   - Use um loop for para exibir os números de 1 a 10 no console.
+   - Use um loop while para contar de 10 a 1 e exibir no console.
+
+5. *Funções:*
+   - Crie uma função que calcule a média de três números.
+   - Escreva uma função que converta graus Celsius para Fahrenheit.
+
+6. *Arrays:*
+   - Declare um array com nomes de frutas e liste-os no console.
+   - Adicione e remova elementos de um array.
+
+7. *Objetos:*
+   - Crie um objeto representando uma pessoa com propriedades como nome, idade e cidade.
+   - Acesse e modifique propriedades de um objeto.
+
+   8. *Manipulação de DOM:*
+   - Crie uma página HTML com um botão que exibe uma mensagem ao ser clicado usando JavaScript.
+   - Modifique o estilo de um elemento HTML através do JavaScript.
+
+9. *Eventos:*
+   - Adicione um evento de clique a um botão e realize uma ação quando ele for clicado.
+   - Crie um evento que responda a pressionar uma tecla no teclado.
+
+10. *Manipulação de Strings:*
+    - Concatene strings para criar uma frase.
+    - Extraia partes de uma string usando métodos como substring ou slice.
