d79a7602f0b59d096da0f20fbbf7c29ec3a03799
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-30 09:42:37 -0300

    Iniciei ex07.js

---
+++ exerciciosExtras/Exercicios 02/ex07.js
@@ -0,0 +1,13 @@
+/*
+7. *Objetos:*
+   - Crie um objeto representando uma pessoa com propriedades como nome, idade e cidade.
+   - Acesse e modifique propriedades de um objeto.
+*/
+
+let pessoa = {
+    nome: 'João',
+    idade: 21,
+    cidade: 'Fortaleza'
+}
+
+console.log(pessoa)
